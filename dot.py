f2fb7d638ad3e0f73ceb4e731799f10d3771c1c3
Sai Udheep <2023ucp1971@mnit.ac.in>
2024-10-14 15:32:40 +0530

    After stash i commited this new change

--- dot.py
+++ dot.py
@@ -3,4 +3,4 @@ j1=int(input("Enter j coefficient of 1st vector"))
 i2=int(input("Enter i coefficient of 2nd vector"))
 j2=int(input("Enter j coefficient of 2nd vector"))
 print("Dot product is ",(i1*i2)+(j1*j2),"\n")
-
+#new modif
