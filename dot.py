b2108c543c789f90eaaa443d07928f25018ca8d7
Sai Udheep <2023ucp1971@mnit.ac.in>
2024-10-14 14:48:58 +0530

    Adding dot product code to rep

---
+++ dot.py
@@ -0,0 +1,5 @@
+i1=int(input("Enter i coefficient of 1st vector"))
+j1=int(input("Enter j coefficient of 1st vector"))
+i2=int(input("Enter i coefficient of 2nd vector"))
+j2=int(input("Enter j coefficient of 2nd vector"))
+print("Dot product is ",i1*i2,"i + ",j1*j2,"j\n")
