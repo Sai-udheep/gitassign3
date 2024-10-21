4c3647357cd53f1c588dfe310a457d3bb2c1fcfe
Sai Udheep <2023ucp1971@mnit.ac.in>
2024-10-21 14:44:41 +0530

    This is after pushing

--- dot.py
+++ dot.py
@@ -3,4 +3,5 @@ j1=int(input("Enter j coefficient of 1st vector"))
 i2=int(input("Enter i coefficient of 2nd vector"))
 j2=int(input("Enter j coefficient of 2nd vector"))
 print("Dot product is ",(i1*i2)+(j1*j2),"\n")
+print("Hello this is for pushing")
 #new modif
